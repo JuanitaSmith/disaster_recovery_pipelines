71dd2a3c656c48f497c152b9e975aa1fda829dc4
Juanita Smith <juanita_smith@goodyear.com>
2024-09-30 14:09:03 +0100

    tests correct errors

--- tests/test_train_classifier.py
+++ tests/test_train_classifier.py
@@ -10,6 +10,9 @@ import numpy as np
 from src import config
 from src.train_classifier import load_data, split_data, build_model, FocalBinaryLoss, RANDOM_STATE
 
+import warnings
+warnings.filterwarnings('ignore')
+
 
 class TestTrainClassifierClass(unittest.TestCase):
 
@@ -24,7 +27,7 @@ class TestTrainClassifierClass(unittest.TestCase):
     def test_split_data(self):
         """ Make sure split into training and testing datasets are working"""
         X, y, category_names = load_data(config.path_database)
-        X_train, y_train, X_val, y_val, X_test, y_test = split_data(X, y, random_state=RANDOM_STATE)
+        X_train, y_train, X_test, y_test = split_data(X, y, random_state=RANDOM_STATE)
         self.assertGreaterEqual(X_train.shape[0], X.shape[0], 'X_train oversampling did not work')
         self.assertEqual(X_train.shape[0], y_train.shape[0],
                          'Training datasets should have the same number of rows.')
@@ -37,12 +40,12 @@ class TestTrainClassifierClass(unittest.TestCase):
 
 
     def test_build_model(self):
-        """ Is focal loss function actually working? This test will run for around 5-10 minutes """
+        """ Is focal loss function actually working? """
 
         model = build_model()
         focal_loss = FocalBinaryLoss(gamma=10)
         X, y, category_names = load_data(config.path_database)
-        X_train, y_train, X_val, y_val, X_test, y_test = split_data(X, y, random_state=RANDOM_STATE)
+        X_train, y_train, X_test, y_test = split_data(X, y, random_state=RANDOM_STATE)
         model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
         loss = focal_loss.focal_binary_cross_entropy(y_pred, np.array(y_test))
